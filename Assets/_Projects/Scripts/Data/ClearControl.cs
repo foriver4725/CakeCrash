7ac8e84e56440ddc6531c13b39f457ae5c6fd96a
AbeJunsei <153270000+Ajun5GIT@users.noreply.github.com>
2024-09-18 23:41:17 +0900

    feature: 時間に関連する機能の追加
    1 ゲームクリアに関するクラス
    2 ディレクショナルライト(太陽)に関するクラス
    3 時間管理に関するクラス

---
+++ Assets/_Projects/Scripts/Data/ClearControl.cs
@@ -0,0 +1,55 @@
+﻿using Interface;
+using System;
+using static UnityEngine.Debug;
+
+namespace Handler.Main.TimeRelated 
+{
+    /// <summary>
+    /// ゲームクリアに関するクラス
+    /// </summary>
+    internal sealed class ClearControl : IDisposable, INullExistable, IEventable
+    {
+
+        /// <summary>
+        /// Start()で呼ぶ
+        /// </summary>
+        public void Start()
+        {
+            if (IsNullExist()) return;
+        }
+
+        /// <summary>
+        /// Update()で呼ぶ
+        /// </summary>
+        public void Update()
+        {
+            if (IsNullExist()) return;
+        }
+
+        /// <summary>
+        /// ゲームクリア時の処理
+        /// </summary>
+        internal void GameClear()
+        {
+            Log("ゲームクリア");
+        }
+
+        /// <summary>
+        /// null代入などの破棄処理
+        /// </summary>
+        public void Dispose()
+        {
+
+        }
+
+        /// <summary>
+        /// nullメンバが存在するか、またはnullメンバを持つメンバがいるかを確認する処理
+        /// </summary>
+        public bool IsNullExist()
+        {
+            return false;
+        }
+    }
+}
+
+
