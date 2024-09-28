0486b519e720cd8f8a1ee3d9fb1657a7a8a55f34
foriver4725 <hrt67@icloud.com>
2024-09-28 20:38:10 +0900

    feature: タイトル画像差し替え、始めるボタンのアニメーション
    2Dスプライトパッケージインストール

---
+++ Assets/_Projects/Scripts/Data/Title/TitleImage/StartImageProperty.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+namespace Data.Title.TitleImage
+{
+    [Serializable]
+    internal sealed class StartImageProperty
+    {
+        [SerializeField, Range(1, 10), Header("‰½’iŠK‚É•ª‚¯‚é‚©")]
+        private int lightStep;
+        internal int LightStep => lightStep;
+
+        [SerializeField, Range(0.1f, 10.0f), Header("‰½•b‚Å1’iŠKi‚Þ‚©")]
+        private float lightDuration;
+        internal float LightDuration => lightDuration;
+
+        internal static readonly float LightStartFillAmount = 0.0f;
+        internal static readonly float LightEndFillAmount = 1.0f;
+    }
+}
